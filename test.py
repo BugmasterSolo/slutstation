9bdc2c03f5d03c49c4fc612f22a7f34665f1d78e
jamieboy1337 <43399648+jamieboy1337@users.noreply.github.com>
2019-04-07 11:27:51 -0700

    first upl

--- test.py
+++ test.py
@@ -1,11 +1,31 @@
-import discord
+from discord.ext.commands import Bot
+from discord import Game
+from os import listdir
 
 BOT_PREFIX = ("?", "!")
 
-TOKEN = "NTY0MTgwMDkxNjc2OTE3Nzgx.XKkImg.XsPRPXS2QhQidXZKc-4fuiTC7aI"
+bot = Bot(command_prefix='g ', description="sorry")
 
-client = Bot(command_prefix=BOT_PREFIX)
+def get_token():
+    with open('secret_token.txt', 'r') as t:
+        return t.read().strip()
+    
+TOKEN = get_token()
 
-# bot object, vs. client implementation.
+@bot.event
+async def on_ready():
+    print('Up and running!')
+    await bot.change_premise(game=Game(name='like damn dude'))
 
-client.run(TOKEN)
+@bot.command(name='test')
+async def tester(ctx):
+    await ctx.send('fuck off')
+
+@bot.command(name='concat')
+# absorbs args into a single one
+# otherwise rest param absorbs word by word
+async def testerTwo(ctx, *, arg):
+    await ctx.send(arg)
+
+bot.run(TOKEN)
+    
