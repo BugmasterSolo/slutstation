a1d39bce0c9b6c73d16afe910f4b8653ea5ec4d1
jamieboy1337 <43399648+jamieboy1337@users.noreply.github.com>
2019-04-07 17:51:27 -0700

    okay

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ BOT_PREFIX = ("?", "!")
 # todo:
 #   - start importing cogs to break up the bot flow
 #   - figure out what exactly the bot should do
+#   - store user info somewhere and fetch it (track level, xp, etc ........)
 #   - asasasdadsd
 
 bot = Bot(command_prefix='g ', description="sorry")
@@ -15,15 +16,16 @@ bot = Bot(command_prefix='g ', description="sorry")
 def get_token():
     with open('secret_token.txt', 'r') as t:
         return t.read().strip()
-    
+
 TOKEN = get_token()
 
 @bot.event
 async def on_ready():
     print('Up and running!')
-    await bot.change_presence(status=Status.dnd, activity=Game("mike craft"))
+    await bot.change_presence(status=Status.online, activity=Game("mike craft"))
 
-
+# wrapper: "packages" the command in a way that allows it to keep eyes out for the name,
+#          then passes in the context when necessary.
 @bot.command(name='test')
 # commands accept a context, and the message is sent back thru the context.
 async def tester(ctx):
@@ -32,10 +34,13 @@ async def tester(ctx):
 @bot.command(name='concat')
 # absorbs args into a single one
 # otherwise rest param absorbs word by word
+# **kwargs at the end
 async def testerTwo(ctx, *, arg):
     await ctx.send(arg)
 
 @bot.event
+# i'd imagine this wrapper takes the function name and attaches it to an event based on.
+# the event keeps track of the API, then routes it back to here
 async def on_guild_join(guild):
     # this event accepts a guild only.
     print("Invited to {}: ID {}".format(guild.name, guild.id))
@@ -44,15 +49,13 @@ async def on_guild_join(guild):
         await open_channel.send("Thanks for the invite :)")
     else:
         print("could not post join message.")
-        
 
 def find_channel(guild):
     for c in guild.text_channels:
         if c.permissions_for(guild.me).send_messages:
             return c;
     return None;
-        
-    
+
+
 
 bot.run(TOKEN)
-    
